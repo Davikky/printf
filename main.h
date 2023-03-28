587ca85a5281683c2ef4a97adf70465659b46149
Benedicta-107 <benedictaibeawuchi@gmail.com>
2023-03-28 15:16:00 -0700

    Format handler updated

--- main.h
+++ main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+int _strlen(const char *str);
 int _putchar(char c);
 
+/* handler.c */
+int handler(const char *str, va_list list);
+int percent_handler(const char *str, va_list list, int *i);
 
 
 
