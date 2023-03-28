c400876172f69be46eba2a37ce05a97d5418e25c
Davikky <ayobamivikky@gmail.com>
2023-03-28 16:14:17 -0700

    _putchar.c updated

--- _putchar.c
+++ _putchar.c
@@ -1,14 +1,59 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
 
 /**
- * _putchar - prints a character
- * @c: input char
- * Return: 0
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1, on error -1
  */
 
 int _putchar(char c)
 {
-	putchar(c);
-	return (0);
+	return (buffer(c));
+}
+
+/**
+ * buffer - Save the character in a buffer
+ * @c: Character
+ *
+ * Return: 1 on success, -1 on failure
+ **/
+
+int buffer(char c)
+{
+	static char *buffering = NULL;
+	static int i = 0;
+
+	if (buffering == NULL)
+	{
+		buffering = malloc(sizeof(char) * BUFFER_SIZE);
+		if (buffering == NULL)
+			return (-1);
+	}
+
+	if (c == -1 || i == BUFFER_SIZE)
+	{
+		write(1, buffering, i);
+		i = 0;
+	}
+
+	if (c != -1)
+		buffering[i++] = c;
+
+	return (1);
+}
+
+/**
+ * free_buffer - Frees the memory allocated for the buffer
+ **/
+void free_buffer(void)
+{
+	if (buffering != NULL)
+	{
+		free(buffering);
+		buffering = NULL;
+	}
 }
